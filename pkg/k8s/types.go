5969f82f8a2347d8c0798a16a12bfcf1488a4f73
Muhammad Faizan <m.faizan@sap.com>
2023-04-05 12:19:00 +0200

    added comment

--- pkg/k8s/types.go
+++ pkg/k8s/types.go
@@ -1,7 +1,10 @@
 package k8s
 
 const (
-	DestinationRuleCrdName    string = "destinationrules.networking.istio.io"
-	DestinationRuleKind       string = "DestinationRule"
+	// DestinationRuleCrdName defines the CRD name for DestinationRule as in Istio.
+	DestinationRuleCrdName string = "destinationrules.networking.istio.io"
+	// DestinationRuleKind defines the Kind name for DestinationRule as in Istio.
+	DestinationRuleKind string = "DestinationRule"
+	// DestinationRuleAPIVersion defines the API version for DestinationRule as in Istio.
 	DestinationRuleAPIVersion string = "networking.istio.io/v1alpha3"
 )
