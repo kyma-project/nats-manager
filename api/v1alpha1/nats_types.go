b092bf2b1612bb147c3240f1ed7490a46f0db6b0
Friedrich Wilken <jens.wilken@sap.com>
2023-05-22 20:30:25 +0200

    validate cluster size minimum

--- api/v1alpha1/nats_types.go
+++ api/v1alpha1/nats_types.go
@@ -100,7 +100,7 @@ type NATSSpec struct {
 type Cluster struct {
 	// Size of a NATS cluster, i.e. number of NATS nodes.
 	// +kubebuilder:validation:Required
-	// +kubebuilder:validation:XValidation:rule="self > -1", message="Value must be greater than -1"
+	// +kubebuilder:validation:Minimum:=0
 	Size int `json:"size"`
 }
 
