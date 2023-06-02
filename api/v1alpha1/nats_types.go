456d6f57050163c57962fc80241e7f3d2725251c
Friedrich <jens.wilken@sap.com>
2023-06-02 15:49:48 +0200

    validation for spec.cluster.size (#60)

--- api/v1alpha1/nats_types.go
+++ api/v1alpha1/nats_types.go
@@ -98,7 +98,9 @@ type NATSSpec struct {
 // Cluster defines configurations that are specific to NATS clusters.
 type Cluster struct {
 	// Size of a NATS cluster, i.e. number of NATS nodes.
-	// +kubebuilder:validation:XValidation:rule="( self % 2 ) != 0", message="size only accepts odd numbers"
+	// +optional
+	// +kubebuilder:validation:Minimum:=1
+	// +kubebuilder:validation:XValidation:rule="self%2!=0", message="size only accepts odd numbers"
 	Size int `json:"size"`
 }
 
