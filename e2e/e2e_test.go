4bb2c63d3c304c0629474dbd34b72935e8681571
Friedrich Wilken <jens.wilken@sap.com>
2023-06-22 10:34:02 +0200

    restructure test

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -9,47 +9,109 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
 
+	"github.com/avast/retry-go/v3"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	appsv1 "k8s.io/api/apps/v1"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-const kymaSystem = "kyma-system"
-const eventingNats = "eventing-nats"
+const (
+	kymaSystem   = "kyma-system"
+	eventingNats = "eventing-nats"
+)
 
-func Test_podsHealthy(t *testing.T) {
+const (
+	attempts = 30
+	delay    = 10 * time.Second
+)
+
+var clientset *kubernetes.Clientset
+
+func TestMain(m *testing.M) {
 	userHomeDir, err := os.UserHomeDir()
-	require.NoError(t, err)
+	if err != nil {
+		panic(err)
+	}
 	kubeConfigPath := filepath.Join(userHomeDir, ".kube", "config")
 	fmt.Printf("Using kubeconfig: %s\n", kubeConfigPath)
 
 	kubeConfig, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
-	require.NoError(t, err)
+	if err != nil {
+		panic(err)
+	}
 
-	clientSet, err := kubernetes.NewForConfig(kubeConfig)
-	require.NoError(t, err)
+	clientset, err = kubernetes.NewForConfig(kubeConfig)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Test_namespace_was_created simply tries to get the namespace on the cluster.
+func Test_namespace_was_created(t *testing.T) {
+	t.Parallel()
 
 	ctx := context.TODO()
-	ns, err := clientSet.CoreV1().Namespaces().Get(ctx, kymaSystem, metav1.GetOptions{})
+	err := retry.Do(
+		func() error {
+			var err error
+			_, err = clientset.CoreV1().Namespaces().Get(ctx, kymaSystem, metav1.GetOptions{})
+			if err != nil {
+				return err
+			}
+			return nil
+		},
+		retry.Delay(delay),
+		retry.Attempts(attempts),
+	)
 	require.NoError(t, err)
-	fmt.Printf("found namespace: '%s'", ns.GetName())
+}
+
+func Test_podsHealthy(t *testing.T) {
+	t.Parallel()
 
 	// Get the StatefulSet.
-	sts, err := clientSet.AppsV1().StatefulSets(kymaSystem).Get(ctx, eventingNats, metav1.GetOptions{})
+	ctx := context.TODO()
+	var sts *appsv1.StatefulSet
+	err := retry.Do(
+		func() error {
+			var err error
+			sts, err = clientset.AppsV1().StatefulSets(kymaSystem).Get(ctx, eventingNats, metav1.GetOptions{})
+			if err != nil {
+				return err
+			}
+			return nil
+		},
+		retry.Delay(delay),
+		retry.Attempts(attempts),
+	)
 	require.NoError(t, err)
 
 	// Get the pods via labels.
+	var pods *v1.PodList
 	listOptions := metav1.ListOptions{
 		LabelSelector: "nats_cluster=eventing-nats",
 	}
-	pods, err := clientSet.CoreV1().Pods(kymaSystem).List(ctx, listOptions)
+	err = retry.Do(
+		func() error {
+			pods, err = clientset.CoreV1().Pods(kymaSystem).List(ctx, listOptions)
+			if err != nil {
+				return err
+			}
+			if len(pods.Items) != int(*sts.Spec.Replicas) {
+				return fmt.Errorf("Error fetching Pods; expexted %v but is %v", sts.Spec.Replicas, len(pods.Items))
+			}
+			return nil
+		},
+		retry.Delay(delay),
+		retry.Attempts(attempts),
+	)
 	require.NoError(t, err)
-
-	fmt.Printf("\n the sts has `replicas=%v` and there are %v pods \n", int(*sts.Spec.Replicas), len(pods.Items))
-	assert.Equal(t, int(*sts.Spec.Replicas), len(pods.Items))
 
 	// Check if all Pods are ready (the status.conditions array has an entry with .type="Ready" and the
 	// .status="True").
