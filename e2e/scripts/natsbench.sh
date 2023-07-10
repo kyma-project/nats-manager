145a96d11bb159544b7b6eadf9892873f94572e4
Friedrich Wilken <jens.wilken@sap.com>
2023-07-10 19:35:16 +0200

    changed order in script

--- e2e/scripts/natsbench.sh
+++ e2e/scripts/natsbench.sh
@@ -8,15 +8,17 @@ sleep 1
 # This will will all the port-forwarding and delete the stream. We need this to be in a function so we can even call it,
 # if our tests fails since `set -e` would stop the script in case of an failing test.
 function cleanup() {
-  kill ${PID}
   # Forcefully purge the stream.
   nats stream purge benchstream -f
   # Forcefully delete the stream.
   nats stream rm benchstream -f
+  
+  kill ${PID}
 }
 
 # This kills the port-forwards even if the test fails.
 trap cleanup ERR
+
 CLUSTER_SIZE=$(kubectl get nats -n kyma-system eventing-nats -ojsonpath='{.spec.cluster.size}')
 # The following will run `bench` with the subject 'testsubject', 5 publishers 5 subscribers,
 # 16 byte size per message, 1000 messages using JetStream.
