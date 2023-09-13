7c7f53829c1b3955eee97e2f4038367ea6cba979
Korbinian Stoemmer <korbinian.stoemmer@sap.com>
2023-09-13 14:41:01 +0200

    add release scripts based on kyma-project/keda-manager (#132)
    * add release scripts based on kyma-project/keda-manager
    
    * restore .env file
    
    * add release workflow
    
    * use .version file in both makefiles and all scripts
    
    * update version of next build

---
+++ scripts/check_image.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+REF_NAME="${1:-"main"}"
+RAW_EXPECTED_SHA=$(git log "${REF_NAME}" --max-count 1 --skip 1 --format=format:%H)
+SHORT_EXPECTED_SHA=${RAW_EXPECTED_SHA:0:8}
+DATE="v$(git log "${REF_NAME}" --max-count 1 --skip 1 --format=format:%ad --date=format:'%Y%m%d')"
+EXPECTED_TAG="${DATE}-${SHORT_EXPECTED_SHA}"
+
+IMAGE_TO_CHECK="${2:-europe-docker.pkg.dev/kyma-project/prod/nats-manager}"
+BUMPED_IMAGE_TAG=$(cat sec-scanners-config.yaml | grep "${IMAGE_TO_CHECK}" | cut -d : -f 2)
+
+if [[ "$BUMPED_IMAGE_TAG" != "$EXPECTED_TAG" ]]; then
+  echo "Tags are not correct: wanted $EXPECTED_TAG but got $BUMPED_IMAGE_TAG"
+  exit 1
+fi
+echo "Tags are correct"
+exit 0
