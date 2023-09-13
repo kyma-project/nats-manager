972e3e4c79a330feb9a324b7802e59f6b29ef44f
Korbinian Stoemmer <korbinian.stoemmer@sap.com>
2023-09-13 13:55:56 +0200

    add release scripts based on kyma-project/keda-manager

---
+++ scripts/check_release_tag.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -ue
+
+DESIRED_TAG=$1
+
+source .version
+MODULE_VERSION="v${MODULE_VERSION}"
+
+if [[ "$DESIRED_TAG" != "$MODULE_VERSION" ]]; then
+  echo "Tags mismatch: expected ${MODULE_VERSION}, got $DESIRED_TAG"
+  exit 1
+fi
+echo "Tags are correct"
+exit 0
