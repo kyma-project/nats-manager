676702c1ed1675288e76f06593141fc9ff5ec3c5
Friedrich Wilken <jens.wilken@sap.com>
2023-09-26 10:06:25 +0200

    extend retries

--- e2e/setup/setup_test.go
+++ e2e/setup/setup_test.go
@@ -33,8 +33,8 @@ import (
 
 // Constants for retries.
 const (
-	interval = 2 * time.Second
-	attempts = 60
+	interval = 3 * time.Second
+	attempts = 120
 )
 
 // clientSet is what is used to access K8s build-in resources like Pods, Namespaces and so on.
