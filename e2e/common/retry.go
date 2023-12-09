e235f535a8f1bb5aaa336ba017acc3b163c03946
Friedrich Wilken <friedrichwilken@posteo.net>
2023-12-09 10:45:07 +0100

    Refactor retry to avoid linter warnings.

--- e2e/common/retry.go
+++ e2e/common/retry.go
@@ -8,16 +8,16 @@ func Retry(attempts int, interval time.Duration, fn func() error) error {
 	ticker := time.NewTicker(interval)
 	defer ticker.Stop()
 	var err error
-	for { //nolint:gosimple//There is no range here.
-		select {
-		case <-ticker.C:
-			attempts--
-			err = fn()
-			if err == nil || attempts == 0 {
-				return err
-			}
+
+	for attempts > 0 {
+		<-ticker.C
+		attempts--
+		err = fn()
+		if err == nil {
+			return nil
 		}
 	}
+	return err
 }
 
 func RetryGet[T any](attempts int, interval time.Duration, fn func() (*T, error)) (*T, error) {
@@ -25,14 +25,14 @@ func RetryGet[T any](attempts int, interval time.Duration, fn func() (*T, error)
 	defer ticker.Stop()
 	var err error
 	var obj *T
-	for { //nolint:gosimple//There is no range here.
-		select {
-		case <-ticker.C:
-			attempts--
-			obj, err = fn()
-			if err == nil || attempts == 0 {
-				return obj, err
-			}
+
+	for attempts > 0 {
+		<-ticker.C
+		attempts--
+		obj, err = fn()
+		if err == nil {
+			return obj, nil
 		}
 	}
+	return obj, err
 }
