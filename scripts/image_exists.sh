b5f91dc31c2d34e9b3f27634a71c074070329aec
Friedrich Wilken <jens.wilken@sap.com>
2023-12-12 19:37:35 +0100

    Check if the release image was created.

---
+++ scripts/image_exists.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+TIMEOUT=${1:-600}
+INTERVAL=${2:-20}
+TAG=${3:-latest}
+IMAGE=${4:-europe-docker.pkg.dev/kyma-project/prod/nats-manager}
+
+start_time=$(date +%s)
+end_time=$((start_time + TIMEOUT))
+
+while true; do
+	docker manifest inspect $IMAGE:$TAG
+	# Check the exit status
+	if [ $? -eq 0 ]; then
+		echo "Image found."
+		exit 0
+	else
+		echo "Image not found, yet."
+	fi
+
+	current_time=$(date +%s)
+
+	# Check if the TIMEOUT has been reached
+	if [ $current_time -ge $end_time ]; then
+		echo "TIMEOUT reached. Image was not found in time."
+		exit 1
+	fi
+
+	sleep $INTERVAL
+done
