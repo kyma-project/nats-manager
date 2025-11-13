03a4e2d564a891ef7595105c00db4d6fd147f109
Małgorzata Świeca <malgorzata.swieca@sap.com>
2025-11-13 10:55:54 +0100

    Update _sidebar.ts

--- docs/user/_sidebar.ts
+++ docs/user/_sidebar.ts
@@ -2,7 +2,7 @@ export default [
   { text: 'Accessing the NATS Server Using CLI', link: './01-10-access-nats-server.md' },
   { text: 'NATS Custom Resource', link: './01-05-nats-custom-resource.md' },
   { text: 'Troubleshooting', link: './troubleshooting/README.md', collapsed: true, items: [
-    { text: 'General Diagnostics', link: './troubleshooting/03-05-nats-troubleshooting.md' },
-    { text: 'Published Events Are Pending in the Stream', link: './troubleshooting/03-10-fix-pending-events.md)' }
+    { text: 'General Diagnostics: NATS Module Readiness and Connectivity', link: './troubleshooting/03-05-nats-troubleshooting.md' },
+    { text: 'Published Events Are Pending in the Stream', link: './troubleshooting/03-10-fix-pending-events.md' }
     ] },
   ]
