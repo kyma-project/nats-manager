1933808cbc6a4b6ccf90ce0e157e6b4bce2c24d9
Małgorzata Świeca <malgorzata.swieca@sap.com>
2025-11-14 14:05:52 +0100

    docs: update links and CODEOWNRES (#609)
    * docs: update links and CODEOWNRES
    
    * Update CODEOWNERS

--- docs/user/_sidebar.ts
+++ docs/user/_sidebar.ts
@@ -1,8 +1,8 @@
 export default [
-  { text: 'Accessing the NATS Server Using CLI', link: './01-10-access-nats-server.md' },
-  { text: 'NATS Custom Resource', link: './01-05-nats-custom-resource.md' },
+  { text: 'Accessing the NATS Server Using CLI', link: './01-10-access-nats-server' },
+  { text: 'NATS Custom Resource', link: './01-05-nats-custom-resource' },
   { text: 'Troubleshooting', link: './troubleshooting/README.md', collapsed: true, items: [
-    { text: 'General Diagnostics: NATS Module Readiness and Connectivity', link: './troubleshooting/03-05-nats-troubleshooting.md' },
-    { text: 'Published Events Are Pending in the Stream', link: './troubleshooting/03-10-fix-pending-events.md' }
+    { text: 'General Diagnostics: NATS Module Readiness and Connectivity', link: './troubleshooting/03-05-nats-troubleshooting' },
+    { text: 'Published Events Are Pending in the Stream', link: './troubleshooting/03-10-fix-pending-events' }
     ] },
   ]
