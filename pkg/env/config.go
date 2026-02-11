aeba8ff44c17836c430a9a955ecb966796f82ff3
Ivan Jovovic <ivan.jovovic@sap.com>
2026-02-11 14:44:13 +0100

    read fips and nats env-vars into config

--- pkg/env/config.go
+++ pkg/env/config.go
@@ -10,6 +10,9 @@ type Config struct {
 	NATSChartDir    string `envconfig:"NATS_CHART_DIR"    required:"true"`
 	NATSCRName      string `envconfig:"NATS_CR_NAME"      required:"true"`
 	NATSCRNamespace string `envconfig:"NATS_CR_NAMESPACE" required:"true"`
+	FIPSModeEnabled string `envconfig:"KYMA_FIPS_MODE_ENABLED" default:"false"`
+	NATSImage       string `envconfig:"NATS_IMAGE"        required:"true"`
+	NATSImageFIPS   string `envconfig:"NATS_IMAGE_FIPS"   required:"true"`
 }
 
 func GetConfig() (Config, error) {
